d3d8421c322c70bdf2320818771a085280453e0e
YayaBou <elias.bougj@hotmail.fr>
2019-02-03 23:52:53 +0100

    Supression du mode debug

--- Flask/Vidalapp/views.py
+++ Flask/Vidalapp/views.py
@@ -10,19 +10,22 @@ app.config.from_object(config)
 
 @app.route('/', methods=['GET','POST'])
 def accueil():
+    #Appel elasticsearch avec les données du form remplie par l'utilisateur
     if request.method == 'POST':
         sub = request.form.get("substance").lower()
         exc = request.form.get("excipient").lower()
+        #Vérification si la donnée excipient est présente
         if exc=="":
             exc=False
         substance  = elastic.search_insubstance(sub)
         medicament = utils.reshape_to_3_columns(elastic.search_inmedicament(sub, exc))
 
         return render_template('real-app.html', substance = substance, medicament = medicament)
-
+    #Si l'appel est GET
     return render_template('real-app.html')
 
 
+#Root de test
 @app.route('/test', methods=['GET', 'POST'])
 def test():
     if request.method == 'POST':
@@ -30,4 +33,4 @@ def test():
     return '<form action="" method="post"><input type="text" name="msg" /><input type="submit" value="Envoyer" /></form>'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
